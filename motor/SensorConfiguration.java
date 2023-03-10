b8ece1f424496f920e8f1078adc92010b65dee29
Jeff Spragg <jwspragg@mtu.edu>
2023-03-09 18:12:10 -0800

    Formatter

--- motor/SensorConfiguration.java
+++ motor/SensorConfiguration.java
@@ -4,45 +4,37 @@ import com.ctre.phoenix.sensors.CANCoder;
 import com.ctre.phoenix.sensors.SensorTimeBase;
 
 /**
- * SensorConfiguration presents an abstracted interface for configuring sensor
- * values used on motors.
+ * SensorConfiguration presents an abstracted interface for configuring sensor values used on motors.
  * 
  * There are three classes of encoder sensors available:
  * <ul>
- * <li>IntegratedSensorSource: A sensor integral to the motor controller, only
- * supported on TalonFX (Falcon 500 motor) and SparkMax controllers.</li>
- * <li>ConnectedSensorSource: A sensor that is external to the controller, but
- * connected directly to it, for example using the SRX port on a TalonSRX.</li>
- * <li>ExternalSensorSource: A sensor that is not connected to the motor
- * controller, but instead connects to the RoboRio. Anything that extends the
- * WPILib Encoder class is usable in this mode.</li>
+ * <li>IntegratedSensorSource: A sensor integral to the motor controller, only supported on TalonFX (Falcon 500 motor)
+ * and SparkMax controllers.</li>
+ * <li>ConnectedSensorSource: A sensor that is external to the controller, but connected directly to it, for example
+ * using the SRX port on a TalonSRX.</li>
+ * <li>ExternalSensorSource: A sensor that is not connected to the motor controller, but instead connects to the
+ * RoboRio. Anything that extends the WPILib Encoder class is usable in this mode.</li>
  * </ul>
  * 
  * <p>
- * Note: all three configurations support an "outputGearRatio" parameter that is
- * applied to the setpoint of the motor as a multiplier. This should be set to
- * the effective overall gear ratio between the sensor (or motor, in the case of
- * the Integrated sensor) and the mechanism itself.
+ * Note: all three configurations support an "outputGearRatio" parameter that is applied to the setpoint of the motor as
+ * a multiplier. This should be set to the effective overall gear ratio between the sensor (or motor, in the case of the
+ * Integrated sensor) and the mechanism itself.
  * </p>
  * <p>
- * For example, if this motor is connected to a
- * gearbox that reduces the output 5x (i.e. a
- * 5:1 ratio), then outputGearRatio should be 5. Note: if an overdrive is applied,
- * this value should be a fraction, not a
- * negative number. For example, if the output is geared to increase the output
- * 2x (i.e. a 2:1 ratio), this value should
- * be 0.5, not -2.
+ * For example, if this motor is connected to a gearbox that reduces the output 5x (i.e. a 5:1 ratio), then
+ * outputGearRatio should be 5. Note: if an overdrive is applied, this value should be a fraction, not a negative
+ * number. For example, if the output is geared to increase the output 2x (i.e. a 2:1 ratio), this value should be 0.5,
+ * not -2.
  * </p>
  * 
  * <p>
- * If the sensor is connected directly to the output, e.g. mounted directly on a
- * shooter output shaft, then outputGearRatio
- * should be 1.0.
+ * If the sensor is connected directly to the output, e.g. mounted directly on a shooter output shaft, then
+ * outputGearRatio should be 1.0.
  * </p>
  * 
  * <p>
- * If you use ConnectedSensorSource or ExternalSensorSource, you will need to
- * configure the encoder type separately.
+ * If you use ConnectedSensorSource or ExternalSensorSource, you will need to configure the encoder type separately.
  * </p>
  * 
  * <h3>Example Sensor Configurations</h3>
@@ -60,8 +52,8 @@ import com.ctre.phoenix.sensors.SensorTimeBase;
  * motor.set(ControlMode.VELOCITY, 800);
  * </pre>
  * 
- * <h4>TalonSRX powered arm with 4:1 reduction before the sensor, and 23.4:1
- * reduction after the sensor, using external sensor</h4>
+ * <h4>TalonSRX powered arm with 4:1 reduction before the sensor, and 23.4:1 reduction after the sensor, using external
+ * sensor</h4>
  * 
  * <pre>
  * var motorConfig = ...;
@@ -86,19 +78,17 @@ public class SensorConfiguration {
   public SensorConfiguration(SensorSource source) {
     this.source = source;
   }
-  
+
   public SensorSource source() {
     return source;
   }
 
   /**
-   * IntegratedSensorSource configures the motor controller to use the integrated
-   * sensor available on a motor for the
+   * IntegratedSensorSource configures the motor controller to use the integrated sensor available on a motor for the
    * sensing source.
    * 
-   * Only the SparkMax (in brushless mode) and TalonFX support this. To use a
-   * sensor connected directly to a TalonSRX, use
-   * an instance of ExternalSensorSource.
+   * Only the SparkMax (in brushless mode) and TalonFX support this. To use a sensor connected directly to a TalonSRX,
+   * use an instance of ExternalSensorSource.
    */
   public static class IntegratedSensorSource implements SensorSource {
     public final double outputGearRatio;
@@ -107,15 +97,14 @@ public class SensorConfiguration {
       this.outputGearRatio = outputGearRatio;
     }
   }
-  
+
   /**
-   * ExternalSensorSource configures the motor controller to use sensors connected
-   * to the RoboRIO or provided from other sources.
+   * ExternalSensorSource configures the motor controller to use sensors connected to the RoboRIO or provided from other
+   * sources.
    * 
    * <p>
-   * This requires an instance of the WPILib Encoder class to be provided, which
-   * is used as the source for the sensor. Note that using ExternalSensorSource
-   * will force the library to resort to software based PID control, instead of
+   * This requires an instance of the WPILib Encoder class to be provided, which is used as the source for the sensor.
+   * Note that using ExternalSensorSource will force the library to resort to software based PID control, instead of
    * being able to use the PID control built into the motor controller itself.
    * </p>
    */
@@ -130,9 +119,8 @@ public class SensorConfiguration {
   }
 
   /**
-   * ConnectedSensorSource configures the motor controller to use sensors
-   * connected directly to the motor controller (only supported by the TalonSRX
-   * and SparkMAX).
+   * ConnectedSensorSource configures the motor controller to use sensors connected directly to the motor controller
+   * (only supported by the TalonSRX and SparkMAX).
    */
   public static class ConnectedSensorSource implements SensorSource {
     public final int countsPerRev;
@@ -157,10 +145,7 @@ public class SensorConfiguration {
   }
 
   public static enum ConnectedSensorType {
-    MAG_ENCODER_ABSOLUTE,
-    MAG_ENCODER_RELATIVE,
-    PWM_ENCODER,
-    QUAD_ENCODER
+    MAG_ENCODER_ABSOLUTE, MAG_ENCODER_RELATIVE, PWM_ENCODER, QUAD_ENCODER
   }
 
   // ------- External Sensor types -------
@@ -195,8 +180,8 @@ public class SensorConfiguration {
     public Encoder(edu.wpi.first.wpilibj.Encoder enc, int countsPerRev) {
       this.enc = enc;
 
-      enc.setDistancePerPulse(1.0/countsPerRev);
-    } 
+      enc.setDistancePerPulse(1.0 / countsPerRev);
+    }
 
     public void setPosition(double position) {
       offset = enc.getDistance() - (position / 360.0);
@@ -212,8 +197,7 @@ public class SensorConfiguration {
   }
 
   /**
-   * SensorSource describes what source the motor should use for closed loop
-   * control, if any at all.
+   * SensorSource describes what source the motor should use for closed loop control, if any at all.
    */
   public static interface SensorSource {
     // TODO: remote
@@ -223,7 +207,9 @@ public class SensorConfiguration {
 
   public static interface ExternalSensor {
     public void setPosition(double position);
+
     public double position();
+
     public double velocity();
   }
 }
