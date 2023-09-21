eaee526e813fce6b1317e319f22b7c5e9eb91294
OneLemonyBoi <onelemonyboii@gmail.com>
2023-09-20 21:39:31 -0700

    Add POVButton Compatibility

--- ConstantButton.java
+++ ConstantButton.java
@@ -1,7 +1,9 @@
 package frc.sorutil;
 
 import java.util.function.BooleanSupplier;
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import edu.wpi.first.wpilibj2.command.button.POVButton;
 
 public class ConstantButton {
   private final int stick;
@@ -14,6 +16,10 @@ public class ConstantButton {
 
   public JoystickButton get() {
     return new JoystickButton(ConstantInput.get().lazyJoy(stick), idx);
+  }
+
+  public POVButton getPOV() {
+    return new POVButton(ConstantInput.get().lazyJoy(stick), idx);
   }
 
   public BooleanSupplier supplier() {
