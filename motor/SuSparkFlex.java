12dc25f2e9828ac1954923f11548ee2b9b5b2372
OneLemonyBoi <onelemonyboii@gmail.com>
2024-01-06 00:04:18 -0800

    Update Spark Flex & Max Code

--- motor/SuSparkFlex.java
+++ motor/SuSparkFlex.java
@@ -1,6 +1,6 @@
 package frc.sorutil.motor;
 
-import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkFlex;
 import com.revrobotics.MotorFeedbackSensor;
 import com.revrobotics.REVLibError;
 import com.revrobotics.RelativeEncoder;
@@ -15,12 +15,12 @@ import frc.sorutil.motor.SensorConfiguration.ConnectedSensorSource;
 import frc.sorutil.motor.SensorConfiguration.ExternalSensorSource;
 import frc.sorutil.motor.SensorConfiguration.IntegratedSensorSource;
 
-public class SuSparkMax extends SuController {
+public class SuSparkFlex extends SuController {
   private static final double STALL_LIMIT = 30;
   private static final double DEFAULT_CURRENT_LIMIT = 70;
   private static final int ANALOG_SAMPLE_DEPTH = 16;
 
-  private final CANSparkMax sparkMax;
+  private final CANSparkFlex sparkFlex;
 
   private SuController.ControlMode lastMode;
   private double lastSetpoint;
@@ -30,52 +30,52 @@ public class SuSparkMax extends SuController {
   private SparkAbsoluteEncoder analogSensor;
   private RelativeEncoder digitalSensor;
 
-  public SuSparkMax(CANSparkMax sparkMax, String name, MotorConfiguration motorConfig,
+  public SuSparkFlex(CANSparkFlex sparkFlex, String name, MotorConfiguration motorConfig,
       SensorConfiguration sensorConfig) {
-    super(sparkMax, motorConfig, sensorConfig,
-        java.util.logging.Logger.getLogger(String.format("SparkMAX(%d: %s)", sparkMax.getDeviceId(), name)), name);
+    super(sparkFlex, motorConfig, sensorConfig,
+        java.util.logging.Logger.getLogger(String.format("sparkFlex(%d: %s)", sparkFlex.getDeviceId(), name)), name);
 
-    this.sparkMax = sparkMax;
+    this.sparkFlex = sparkFlex;
 
     configure(motorConfig, sensorConfig);
     initializeLogNames();
-    aLogger.recordOutput(loggerPrefix + "ID", sparkMax.getDeviceId());
+    aLogger.recordOutput(loggerPrefix + "ID", sparkFlex.getDeviceId());
   }
 
   @Override
   protected void configure(MotorConfiguration config, SensorConfiguration sensorConfig) {
-    Errors.handleRev(sparkMax.restoreFactoryDefaults(), logger, "resetting motor config");
+    Errors.handleRev(sparkFlex.restoreFactoryDefaults(), logger, "resetting motor config");
 
-    // This is disabled due to weird interactions on the SparkMax that seems to
+    // This is disabled due to weird interactions on the sparkFlex that seems to
     // affect position control.
 
-    // sparkMax.enableVoltageCompensation(12);
+    // sparkFlex.enableVoltageCompensation(12);
     // if (!config.voltageCompenstationEnabled()) {
-    // sparkMax.disableVoltageCompensation();
+    // sparkFlex.disableVoltageCompensation();
     // }
 
-    sparkMax.setInverted(config.inverted());
+    sparkFlex.setInverted(config.inverted());
 
-    Errors.handleRev(sparkMax.getPIDController().setP(config.pidProfile().p()), logger, "setting P constant");
-    Errors.handleRev(sparkMax.getPIDController().setI(config.pidProfile().i()), logger, "setting I constant");
-    Errors.handleRev(sparkMax.getPIDController().setD(config.pidProfile().d()), logger, "setting D constant");
-    Errors.handleRev(sparkMax.getPIDController().setFF(config.pidProfile().f()), logger, "setting F constant");
+    Errors.handleRev(sparkFlex.getPIDController().setP(config.pidProfile().p()), logger, "setting P constant");
+    Errors.handleRev(sparkFlex.getPIDController().setI(config.pidProfile().i()), logger, "setting I constant");
+    Errors.handleRev(sparkFlex.getPIDController().setD(config.pidProfile().d()), logger, "setting D constant");
+    Errors.handleRev(sparkFlex.getPIDController().setFF(config.pidProfile().f()), logger, "setting F constant");
 
     double limit = DEFAULT_CURRENT_LIMIT;
     if (config.currentLimit() != null) {
       limit = config.currentLimit();
     }
 
-    Errors.handleRev(sparkMax.setSmartCurrentLimit((int) STALL_LIMIT, (int) limit), logger, "setting current limit");
+    Errors.handleRev(sparkFlex.setSmartCurrentLimit((int) STALL_LIMIT, (int) limit), logger, "setting current limit");
 
-    CANSparkMax.IdleMode desiredMode = CANSparkMax.IdleMode.kCoast;
+    CANSparkFlex.IdleMode desiredMode = CANSparkFlex.IdleMode.kCoast;
     if (config.idleMode() == SuController.IdleMode.BRAKE) {
-      desiredMode = CANSparkMax.IdleMode.kBrake;
+      desiredMode = CANSparkFlex.IdleMode.kBrake;
     }
-    Errors.handleRev(sparkMax.setIdleMode(desiredMode), logger, "setting idle mode");
+    Errors.handleRev(sparkFlex.setIdleMode(desiredMode), logger, "setting idle mode");
 
     // TODO: this should be improved to support multiple PID controllers.
-    Errors.handleRev(sparkMax.getPIDController().setOutputRange(-config.maxOutput(), config.maxOutput()), logger,
+    Errors.handleRev(sparkFlex.getPIDController().setOutputRange(-config.maxOutput(), config.maxOutput()), logger,
         "setting neutral deadband");
 
     if (sensorConfig != null) {
@@ -87,7 +87,7 @@ public class SuSparkMax extends SuController {
           case QUAD_ENCODER:
             // fallthrough
           case MAG_ENCODER_RELATIVE:
-            var alt = sparkMax.getAlternateEncoder(connected.countsPerRev);
+            var alt = sparkFlex.getExternalEncoder(connected.countsPerRev);
             alt.setPositionConversionFactor(360); // Convert revolutions to degrees
             digitalSensor = alt;
             sensor = alt;
@@ -95,16 +95,16 @@ public class SuSparkMax extends SuController {
           case MAG_ENCODER_ABSOLUTE:
             // fallthrough
           case PWM_ENCODER:
-            //var analog = sparkMax.getAnalog(Mode.kAbsolute);
-            var analog = sparkMax.getAbsoluteEncoder(SparkAbsoluteEncoder.Type.kDutyCycle);
+            //var analog = sparkFlex.getAnalog(Mode.kAbsolute);
+            var analog = sparkFlex.getAbsoluteEncoder(SparkAbsoluteEncoder.Type.kDutyCycle);
             analog.setAverageDepth(ANALOG_SAMPLE_DEPTH);
             analog.setPositionConversionFactor(360); // Convert revolutions to degrees
             analog.setAverageDepth(32);
 
             // Configure the sensor to continuously rotate through 360 degrees.
-            sparkMax.getPIDController().setPositionPIDWrappingEnabled(true);
-            sparkMax.getPIDController().setPositionPIDWrappingMinInput(0);
-            sparkMax.getPIDController().setPositionPIDWrappingMaxInput(360);
+            sparkFlex.getPIDController().setPositionPIDWrappingEnabled(true);
+            sparkFlex.getPIDController().setPositionPIDWrappingMinInput(0);
+            sparkFlex.getPIDController().setPositionPIDWrappingMaxInput(360);
             
             analogSensor = analog;
             sensor = analog;
@@ -115,7 +115,7 @@ public class SuSparkMax extends SuController {
 
         Errors.handleRev(sensor.setInverted(connected.inverted()), logger, "setting inversion of connected sensor");
 
-        Errors.handleRev(sparkMax.getPIDController().setFeedbackDevice(sensor), logger,
+        Errors.handleRev(sparkFlex.getPIDController().setFeedbackDevice(sensor), logger,
             "setting feedback device to sensor device");
       }
 
@@ -124,19 +124,19 @@ public class SuSparkMax extends SuController {
       }
 
       if (sensorConfig.source() instanceof IntegratedSensorSource) {
-        Errors.handleRev(sparkMax.getPIDController().setFeedbackDevice(sparkMax.getEncoder()), logger,
+        Errors.handleRev(sparkFlex.getPIDController().setFeedbackDevice(sparkFlex.getEncoder()), logger,
             "setting feedback device to integral device");
-        sparkMax.getEncoder().setPositionConversionFactor(360); // Convert revolutions to degrees
-        sparkMax.getEncoder().setVelocityConversionFactor(1);
+        sparkFlex.getEncoder().setPositionConversionFactor(360); // Convert revolutions to degrees
+        sparkFlex.getEncoder().setVelocityConversionFactor(1);
       }
     }
 
-    Errors.handleRev(sparkMax.burnFlash(), logger, "saving settings to onboard Flash");
+    Errors.handleRev(sparkFlex.burnFlash(), logger, "saving settings to onboard Flash");
   }
 
   @Override
   public MotorController rawController() {
-    return sparkMax;
+    return sparkFlex;
   }
 
   boolean canDisconnectGuard = false;
@@ -146,9 +146,9 @@ public class SuSparkMax extends SuController {
   public void tick() {
     super.tick();
 
-    if (lastCode != sparkMax.getLastError()) {
-      Errors.handleRev(sparkMax.getLastError(), logger, "in motor loop, likely from setting a motor update");
-      lastCode = sparkMax.getLastError();
+    if (lastCode != sparkFlex.getLastError()) {
+      Errors.handleRev(sparkFlex.getLastError(), logger, "in motor loop, likely from setting a motor update");
+      lastCode = sparkFlex.getLastError();
     }
 
     if (softPidControllerEnabled) {
@@ -156,12 +156,12 @@ public class SuSparkMax extends SuController {
         // velocity mode
         double current = ((ExternalSensorSource) sensorConfig.source()).sensor.velocity();
         double output = softPidController.calculate(current);
-        sparkMax.set(output + (lastArbFf / Constants.NOMINAL_VOLTAGE));
+        sparkFlex.set(output + (lastArbFf / Constants.NOMINAL_VOLTAGE));
       } else {
         // position mode
         double current = ((ExternalSensorSource) sensorConfig.source()).sensor.position();
         double output = softPidController.calculate(current);
-        sparkMax.set(output + (lastArbFf / Constants.NOMINAL_VOLTAGE));
+        sparkFlex.set(output + (lastArbFf / Constants.NOMINAL_VOLTAGE));
       }
     }
 
@@ -188,7 +188,7 @@ public class SuSparkMax extends SuController {
 
     switch (mode) {
       case PERCENT_OUTPUT:
-        sparkMax.set(setpoint);
+        sparkFlex.set(setpoint);
         break;
       case POSITION:
         setPosition(setpoint, arbFfVolts);
@@ -197,7 +197,7 @@ public class SuSparkMax extends SuController {
         setVelocity(setpoint, arbFfVolts);
         break;
       case VOLTAGE:
-        Errors.handleRev(sparkMax.getPIDController().setReference(setpoint, ControlType.kVoltage), logger,
+        Errors.handleRev(sparkFlex.getPIDController().setReference(setpoint, ControlType.kVoltage), logger,
             "setting motor output");
         break;
     }
@@ -211,24 +211,24 @@ public class SuSparkMax extends SuController {
       double output = motorDegrees;
 
       if (arbFfVolts == 0) {
-        Errors.handleRev(sparkMax.getPIDController().setReference(output, ControlType.kPosition),
+        Errors.handleRev(sparkFlex.getPIDController().setReference(output, ControlType.kPosition),
             logger, "setting motor output");
       } else {
         Errors.handleRev(
-            sparkMax.getPIDController().setReference(output, ControlType.kPosition, 0, arbFfVolts, ArbFFUnits.kVoltage),
+            sparkFlex.getPIDController().setReference(output, ControlType.kPosition, 0, arbFfVolts, ArbFFUnits.kVoltage),
             logger, "setting motor output");
       }
       return;
     }
 
-    // Using sensor external to the SparkMAX.
+    // Using sensor external to the sparkFlex.
     if (sensorConfig.source() instanceof SensorConfiguration.ExternalSensorSource) {
       softPidControllerEnabled = true;
       softPidControllerMode = false;
 
       double current = ((ExternalSensorSource) sensorConfig.source()).sensor.position();
       double output = softPidController.calculate(current, setpoint);
-      sparkMax.set(output + (arbFfVolts / Constants.NOMINAL_VOLTAGE));
+      sparkFlex.set(output + (arbFfVolts / Constants.NOMINAL_VOLTAGE));
       return;
     }
 
@@ -238,11 +238,11 @@ public class SuSparkMax extends SuController {
       double output = motorDegrees;
 
       if (arbFfVolts == 0) {
-        Errors.handleRev(sparkMax.getPIDController().setReference(output, ControlType.kPosition),
+        Errors.handleRev(sparkFlex.getPIDController().setReference(output, ControlType.kPosition),
             logger, "setting motor output");
       } else {
         Errors.handleRev(
-            sparkMax.getPIDController().setReference(output, ControlType.kPosition, 0, arbFfVolts, ArbFFUnits.kVoltage),
+            sparkFlex.getPIDController().setReference(output, ControlType.kPosition, 0, arbFfVolts, ArbFFUnits.kVoltage),
             logger, "setting motor output");
       }
       return;
@@ -258,24 +258,24 @@ public class SuSparkMax extends SuController {
       double output = integrated.outputGearRatio * setpoint;
 
       if (arbFfVolts == 0) {
-        Errors.handleRev(sparkMax.getPIDController().setReference(output, ControlType.kVelocity),
+        Errors.handleRev(sparkFlex.getPIDController().setReference(output, ControlType.kVelocity),
             logger, "setting motor output");
       } else {
         Errors.handleRev(
-            sparkMax.getPIDController().setReference(output, ControlType.kVelocity, 0, arbFfVolts, ArbFFUnits.kVoltage),
+            sparkFlex.getPIDController().setReference(output, ControlType.kVelocity, 0, arbFfVolts, ArbFFUnits.kVoltage),
             logger, "setting motor output");
       }
       return;
     }
 
-    // Using sensor external to the SparkMAX.
+    // Using sensor external to the sparkFlex.
     if (sensorConfig.source() instanceof SensorConfiguration.ExternalSensorSource) {
       softPidControllerEnabled = true;
       softPidControllerMode = true;
 
       double current = ((ExternalSensorSource) sensorConfig.source()).sensor.velocity();
       double output = softPidController.calculate(current, setpoint);
-      sparkMax.set(output + (arbFfVolts / Constants.NOMINAL_VOLTAGE));
+      sparkFlex.set(output + (arbFfVolts / Constants.NOMINAL_VOLTAGE));
       return;
     }
 
@@ -284,11 +284,11 @@ public class SuSparkMax extends SuController {
       double output = connected.outputGearRatio * setpoint;
 
       if (arbFfVolts == 0) {
-        Errors.handleRev(sparkMax.getPIDController().setReference(output, ControlType.kVelocity),
+        Errors.handleRev(sparkFlex.getPIDController().setReference(output, ControlType.kVelocity),
             logger, "setting motor output");
       } else {
         Errors.handleRev(
-            sparkMax.getPIDController().setReference(output, ControlType.kVelocity, 0, arbFfVolts, ArbFFUnits.kVoltage),
+            sparkFlex.getPIDController().setReference(output, ControlType.kVelocity, 0, arbFfVolts, ArbFFUnits.kVoltage),
             logger, "setting motor output");
       }
       return;
@@ -299,31 +299,31 @@ public class SuSparkMax extends SuController {
 
   @Override
   public void stop() {
-    sparkMax.stopMotor();
+    sparkFlex.stopMotor();
     lastSetpoint = 0;
     lastMode = null;
   }
 
   @Override
   public void follow(SuController other) {
-    // Techincally, the spark max can follow other motor controllers, but for now we
+    // Techincally, the spark flex can follow other motor controllers, but for now we
     // will only allow the following of other
-    // SparkMaxes for safety.
+    // sparkFlexes for safety.
 
-    if (!(other.rawController() instanceof CANSparkMax)) {
+    if (!(other.rawController() instanceof CANSparkFlex)) {
       throw new MotorConfigurationError(
-          "SparkMax may only follow other spark max, got instance of: "
+          "Sparkflex may only follow other spark flex, got instance of: "
               + other.rawController().getClass().getName());
     }
 
-    sparkMax.follow((CANSparkMax) other.rawController());
+    sparkFlex.follow((CANSparkFlex) other.rawController());
   }
 
   @Override
   public double outputPosition() {
     if (sensorConfig.source() instanceof IntegratedSensorSource) {
       IntegratedSensorSource source = (IntegratedSensorSource) sensorConfig.source();
-      return sparkMax.getEncoder().getPosition() / source.outputGearRatio;
+      return sparkFlex.getEncoder().getPosition() / source.outputGearRatio;
     }
     if (sensorConfig.source() instanceof ConnectedSensorSource) {
       var source = (ConnectedSensorSource) sensorConfig.source();
@@ -354,7 +354,7 @@ public class SuSparkMax extends SuController {
   public double outputVelocity() {
     if (sensorConfig.source() instanceof IntegratedSensorSource) {
       IntegratedSensorSource source = (IntegratedSensorSource) sensorConfig.source();
-      return sparkMax.getEncoder().getVelocity() / source.outputGearRatio;
+      return sparkFlex.getEncoder().getVelocity() / source.outputGearRatio;
     }
     if (sensorConfig.source() instanceof ConnectedSensorSource) {
       var source = (ConnectedSensorSource) sensorConfig.source();
@@ -385,7 +385,7 @@ public class SuSparkMax extends SuController {
   public void setSensorPosition(double position) {
     if (sensorConfig.source() instanceof IntegratedSensorSource) {
       IntegratedSensorSource source = (IntegratedSensorSource) sensorConfig.source();
-      sparkMax.getEncoder().setPosition(position * source.outputGearRatio);
+      sparkFlex.getEncoder().setPosition(position * source.outputGearRatio);
     }
     if (sensorConfig.source() instanceof ExternalSensorSource) {
       ExternalSensorSource source = (ExternalSensorSource) sensorConfig.source();
@@ -436,10 +436,10 @@ public class SuSparkMax extends SuController {
   }
 
   private void recordLogs() {
-    aLogger.recordOutput(inputVoltageName, sparkMax.getBusVoltage());
-    aLogger.recordOutput(motorOutputName, sparkMax.getAppliedOutput());
-    aLogger.recordOutput(supplyCurrentName, sparkMax.getOutputCurrent());
-    aLogger.recordOutput(motorTemperatureName, sparkMax.getMotorTemperature());
+    aLogger.recordOutput(inputVoltageName, sparkFlex.getBusVoltage());
+    aLogger.recordOutput(motorOutputName, sparkFlex.getAppliedOutput());
+    aLogger.recordOutput(supplyCurrentName, sparkFlex.getOutputCurrent());
+    aLogger.recordOutput(motorTemperatureName, sparkFlex.getMotorTemperature());
     aLogger.recordOutput(arbFfName, lastArbFf);
   }
 }
